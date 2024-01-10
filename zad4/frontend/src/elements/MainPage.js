116d695701f12652207306dc73eeea8de35c2f86
MaciejPerdaszek <maciej.44@wp.pl>
2024-01-10 22:06:11 +0100

    search engine added for mainpage, shopping cart functionality

--- zad4/frontend/src/elements/MainPage.js
+++ zad4/frontend/src/elements/MainPage.js
@@ -5,6 +5,10 @@ export default function MainPage({onAddToCart}) {
 
     const [productsList, setProductsList] = React.useState([]);
     const [productsCart, setProductCart] = React.useState([]);
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [categoriesList, setCategoriesList] = React.useState([]);
+    const [selectedCategory, setSelectedCategory] = React.useState(null);
+
 
     function getProducts() {
         fetch("http://localhost:4000/products")
@@ -13,10 +17,16 @@ export default function MainPage({onAddToCart}) {
             .catch(error => console.log(error));
     }
 
+    function getCategories() {
+        fetch("http://localhost:4000/categories")
+            .then(response => response.json())
+            .then(data => setCategoriesList(data))
+            .catch(error => console.log(error));
+    }
+
     React.useEffect(() => {
         getProducts();
-        console.log(typeof productsCart);
-        console.log(productsCart);
+        getCategories();
         onAddToCart(productsCart);
     }, [productsCart]);
 
@@ -30,6 +40,22 @@ export default function MainPage({onAddToCart}) {
                     </div>
                 </div>
             </div>
+            
+            <div>
+                <label htmlFor="search">Search:</label>
+                    <input type="text" id="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="category">Select category:</label>
+                    <select id="category" value={selectedCategory || ''} onChange={(e) => setSelectedCategory(e.target.value === '' ? null : parseInt(e.target.value, 10))}>
+                        <option value="">All Categories</option>
+                        {categoriesList.map((category) => (
+                            <option key={category.idcategory} value={category.idcategory}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </select>
+            </div>
             <div className="productsTable">
                 <table className="table table-striped">
                     <thead>
@@ -37,15 +63,24 @@ export default function MainPage({onAddToCart}) {
                             <th scope="col">Product name</th>
                             <th scope="col">Description</th>
                             <th scope="col">Price</th>
+                            <th scope="col">Category</th>
                             <th scope="col">Add to cart</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {productsList.map((product, index) => (
+                        {productsList
+                        .filter((product) =>
+                            product.name.toLowerCase().includes(searchTerm.toLowerCase())
+                        )
+                        .filter((product) =>
+                            selectedCategory ? product.idcategory === selectedCategory : true
+                        )
+                        .map((product, index) => (
                             <tr key={index}>
                                 <td>{product.name}</td>
                                 <td>{product.description}</td>
                                 <td>{product.price}</td>
+                                <td>{product.category.name}</td>
                                 <td>
                                     <button className="btn btn-primary" onClick={() => {
                                         const newProductsCart = [...productsCart, product];
--- zad4/frontend/src/elements/ShoppingCart.js
+++ zad4/frontend/src/elements/ShoppingCart.js
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useRef } from "react";
 import '../styling/MainPage.css';
+import { useNavigate } from "react-router-dom";
 
 export default function ShoppingCart({list}) {
 
+    const [amounts, setAmounts] = React.useState({});
+    const formRef = useRef(null);
+    let navigate = useNavigate();
+
+    const handleIncrement = (productId) => {
+        setAmounts((prevAmounts) => ({
+          ...prevAmounts,
+          [productId]: prevAmounts[productId] + 1,
+        }));
+      }; 
+    
+      const handleDecrement = (productId) => {
+        setAmounts((prevAmounts) => ({
+          ...prevAmounts,
+          [productId]: Math.max(0, prevAmounts[productId] - 1),
+        }));
+      };
+
+      const handleRemove = (productId ,index) => {
+        const newProductsCart = [...list];
+        newProductsCart.splice(index, 1);
+        setAmounts((prevAmounts) => {
+            const newAmounts = {...prevAmounts};
+            delete newAmounts[productId];
+            return newAmounts;
+        });
+        list.splice(index, 1);
+      }
+
+    const calclulateTotalPrice = () => {
+        let totalPrice = 0;
+        list.forEach((product) => {
+            totalPrice += product.price * amounts[product.id];
+        });
+        return totalPrice;
+    }
+
+    const submitOrder = (e) => {
+        e.preventDefault();
+        const isFormValid = formRef.current.checkValidity();
+    
+        if (isFormValid) {
+            const nameInput = document.getElementById("name");
+            const emailInput = document.getElementById("email");
+            const phoneInput = document.getElementById("phone");
+            const order = {
+                date: new Date(),
+                status: 1,
+                username: nameInput.value,
+                email: emailInput.value,
+                phone_number: phoneInput.value,
+                products: list
+            };
+            console.log(order);
+            fetch("http://localhost:4000/orders", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(order),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                //navigate('/');
+                console.log("Order submitted successfully");
+                
+            })
+        } else {
+          formRef.current.reportValidity();
+          console.log("Form is invalid!");
+        }
+      };
+        
     React.useEffect(() => {
-        console.log("ShoppingCart - Updated list:", list);
+        const initialAmounts = {};
+        list.forEach((product) => {
+            initialAmounts[product.id] = 0;
+        });
+        setAmounts(initialAmounts);
       }, [list]);
+      
 
     return (
         <div className="container-fluid">
@@ -31,11 +110,36 @@ export default function ShoppingCart({list}) {
                                 <td>{product.name}</td>
                                 <td>{product.description}</td>
                                 <td>{product.price}</td>
+                                <td>
+                                    <button className="btn btn-danger" onClick={() => handleDecrement(product.id)}>-</button>
+                                </td>
+                                <td>{amounts[product.id]}</td>
+                                <td>
+                                    <button className="btn btn-primary" onClick={() => handleIncrement(product.id)}>+</button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-danger" onClick={() => handleRemove(product.id ,index)}>Remove from cart</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
+            <div>
+                <h3>Total Price: {calclulateTotalPrice()}</h3>
+            </div>
+
+            <form ref={formRef}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" placeholder="exampleuser" required />
+  
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" placeholder="example@gmail.com" required />
+
+                <label htmlFor="phone">Phone:</label>
+                <input type="tel" id="phone" pattern="[0-9]{9}" placeholder="123456789" required />
+                <button className="btn btn-primary" onClick={submitOrder}>Submit</button>
+            </form>
         </div>
     )
 }
